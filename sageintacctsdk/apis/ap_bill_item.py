100965edab72788745268021597ea7da8543ede1
Matt Sowden <msowden@consultaegis.com>
2022-11-17 14:09:16 -0500

    use modern fetch

--- sageintacctsdk/apis/ap_bill_item.py
+++ sageintacctsdk/apis/ap_bill_item.py
@@ -10,20 +10,3 @@ class APBillItem(ApiBase):
     """Class for AP Payments APIs."""
     def __init__(self):
         ApiBase.__init__(self, dimension='APBILLITEM')
-
-    def get_all(self):
-        """Get all AP Bill Detail items from Sage Intacct
-
-        Returns:
-            List of Dict in AP Bill Detail schema.
-        """
-        data = {
-            'readByQuery': {
-                'object': 'APBILLITEM',
-                'fields': '*',
-                'query': None,
-                'pagesize': '1000'
-            }
-        }
-
-        return self.format_and_send_request(data)['data']['apbillitem']
