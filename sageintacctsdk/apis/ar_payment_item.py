c7a7fe58a9751d970dfb411d341a4f86ec32da55
Matt Sowden <msowden@consultaegis.com>
2022-12-02 12:35:05 -0500

    arpaymentitem

--- sageintacctsdk/apis/ar_payment_item.py
+++ sageintacctsdk/apis/ar_payment_item.py
@@ -10,20 +10,3 @@ class ARPaymentItem(ApiBase):
     """Class for AR Payments APIs."""
     def __init__(self):
         ApiBase.__init__(self, dimension='ARPAYMENTITEM')
-
-    def get_all(self):
-        """Get all AP Payments from Sage Intacct
-
-        Returns:
-            List of Dict in AP Payments schema.
-        """
-        data = {
-            'readByQuery': {
-                'object': 'ARPAYMENTITEM',
-                'fields': '*',
-                'query': None,
-                'pagesize': '1000'
-            }
-        }
-
-        return self.format_and_send_request(data)['data']['arpaymentitem']
